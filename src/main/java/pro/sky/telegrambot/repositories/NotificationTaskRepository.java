20361e1760b89953a5c1bcc2bd546ff3761897c3
glinka <alexandraglinka@yandex.ru>
2023-10-11 19:40:31 +0300

    master

--- src/main/java/pro/sky/telegrambot/repositories/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repositories/NotificationTaskRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.model.NotificationTask;
 @Repository
-public class NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
 
 }
