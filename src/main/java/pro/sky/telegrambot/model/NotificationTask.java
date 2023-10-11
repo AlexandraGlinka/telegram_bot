3761d6b229d10bc6ca755cce3e43880492c22ee8
glinka <alexandraglinka@yandex.ru>
2023-10-11 19:49:02 +0300

    lombok + @Data

--- src/main/java/pro/sky/telegrambot/model/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -1,5 +1,7 @@
 package pro.sky.telegrambot.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
+@Data // lombok - по умолчанию настраивает конструктор, геттеры, сеттеры итд
 public class NotificationTask {
     @Id
     @GeneratedValue
@@ -15,69 +18,4 @@ public class NotificationTask {
     private String messageText;
     private LocalDateTime messageDate;
 
-    public NotificationTask(Long id, Long userId, String messageText, LocalDateTime messageDate) {
-        this.id = id;
-        this.userId = userId;
-        this.messageText = messageText;
-        this.messageDate = messageDate;
-    }
-
-    public NotificationTask() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getMessageText() {
-        return messageText;
-    }
-
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
-
-    public LocalDateTime getMessageDate() {
-        return messageDate;
-    }
-
-    public void setMessageDate(LocalDateTime messageDate) {
-        this.messageDate = messageDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        NotificationTask that = (NotificationTask) o;
-        return Objects.equals(id, that.id) && Objects.equals(userId, that.userId) && Objects.equals(messageText, that.messageText) && Objects.equals(messageDate, that.messageDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, userId, messageText, messageDate);
-    }
-
-    @Override
-    public String toString() {
-        return "NotificationTask{" +
-                "id=" + id +
-                ", userId=" + userId +
-                ", messageText='" + messageText + '\'' +
-                ", messageDate=" + messageDate +
-                '}';
-    }
 }
