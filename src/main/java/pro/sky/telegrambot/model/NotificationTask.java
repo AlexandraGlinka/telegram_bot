9c3a7b5a843572e3711a6753da753e79084f7c22
glinka <alexandraglinka@yandex.ru>
2023-10-14 13:06:31 +0300

    fix3

--- src/main/java/pro/sky/telegrambot/model/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -12,7 +13,7 @@ import java.util.Objects;
 @Data // lombok - по умолчанию настраивает конструктор, геттеры, сеттеры итд
 public class NotificationTask {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //первичный ключ
     private Long userId; //id пользователя/чата
     private String message;
--- src/main/resources/liquibase/scripts/notification_task.sql
+++ src/main/resources/liquibase/scripts/notification_task.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS notification_task (
-    id serial PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     user_id bigint NOT NULL,
     message character varying(255) NOT NULL,
     message_date TIMESTAMP without time zone
